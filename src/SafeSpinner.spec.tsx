c69475a9a6b853ec1800f725deb250c467f3cc09
Dan Green-Leipciger <dangreen.tlv@gmail.com>
2020-02-15 18:48:54 -0500

    Make it work

--- src/SafeSpinner.spec.tsx
+++ src/SafeSpinner.spec.tsx
@@ -1,14 +1,76 @@
 import * as React from 'react';
-import { render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import SafeSpinner from '.';
 
-test('Should render child component', async () => {
-  const Inner = () => <>hello world</>;
-  const { getByText } = render(
-    <SafeSpinner>
-      <Inner />
-    </SafeSpinner>,
-  );
+beforeAll(() => {
+  jest.useFakeTimers();
+});
 
-  expect(getByText('hello world'));
+afterEach(() => {
+  cleanup();
+});
+
+const Inner = () => <>hello world</>;
+
+describe('SafeSpinner', () => {
+  it('Should render child component', async () => {
+    const { getByText } = render(
+      <SafeSpinner onTimeout={() => {}}>
+        <Inner />
+      </SafeSpinner>,
+    );
+
+    expect(getByText('hello world'));
+  });
+
+  it('Should call the `onTimeout` function at the default timeout', () => {
+    const handleSpinnerTimeout = jest.fn();
+    render(
+      <SafeSpinner onTimeout={handleSpinnerTimeout}>
+        <Inner />
+      </SafeSpinner>,
+    );
+    expect(handleSpinnerTimeout).not.toHaveBeenCalled();
+    jest.advanceTimersByTime(10000);
+    expect(handleSpinnerTimeout).toHaveBeenCalled();
+  });
+
+  it('Should call the `onTimeout` function at the specified timeout (shorter than default)', () => {
+    const handleSpinnerTimeout = jest.fn();
+    render(
+      <SafeSpinner onTimeout={handleSpinnerTimeout} timeout={5000}>
+        <Inner />
+      </SafeSpinner>,
+    );
+    expect(handleSpinnerTimeout).not.toHaveBeenCalled();
+    jest.advanceTimersByTime(5000);
+    expect(handleSpinnerTimeout).toHaveBeenCalled();
+  });
+
+  it('Should call the `onTimeout` function at the specified timeout (longer than default)', () => {
+    const handleSpinnerTimeout = jest.fn();
+    render(
+      <SafeSpinner onTimeout={handleSpinnerTimeout} timeout={15000}>
+        <Inner />
+      </SafeSpinner>,
+    );
+    jest.advanceTimersByTime(10001);
+    expect(handleSpinnerTimeout).not.toHaveBeenCalled();
+    jest.advanceTimersByTime(5000);
+    expect(handleSpinnerTimeout).toHaveBeenCalled();
+  });
+
+  it('Should NOT call the `onTimeout` function if the component unmounted before the timeout', () => {
+    const handleSpinnerTimeout = jest.fn();
+    render(
+      <SafeSpinner onTimeout={handleSpinnerTimeout} timeout={5000}>
+        <Inner />
+      </SafeSpinner>,
+    );
+    expect(handleSpinnerTimeout).not.toHaveBeenCalled();
+    jest.advanceTimersByTime(1000);
+    cleanup();
+    jest.advanceTimersByTime(6000);
+    expect(handleSpinnerTimeout).not.toHaveBeenCalled();
+  });
 });
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,26 @@ import * as React from 'react';
 
 interface ISafeSpinner {
   children: React.ReactElement;
+  /**
+   * Defaults to 10000 ms
+   */
+  timeout?: number;
+  /**
+   * This is called if the spinner is shown for longer than the timeout
+   */
+  onTimeout: () => void;
 }
 
 const SafeSpinner: React.FunctionComponent<ISafeSpinner> = props => {
+  React.useEffect(() => {
+    let timeoutId: number;
+    if (props.onTimeout) {
+      timeoutId = window.setTimeout(props.onTimeout, props.timeout || 10000);
+    }
+    return () => {
+      window.clearTimeout(timeoutId);
+    };
+  });
   return props.children;
 };
 
